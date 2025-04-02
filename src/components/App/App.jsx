0edb7e25128ac93cbd7533749408d009c44142a1
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-04-02 08:30:31 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,16 @@
-import style from './App.module.css';
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../wrappers/Layout/Layout';
+import pages from '../../pages/index';
 
-const App = ({}) => {
-  return <div>App</div>;
+const App = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<pages.Home />} />
+        <Route path="*" element={<pages.Home />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default App;
